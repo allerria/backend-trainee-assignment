d26c9cf1ef474e8c3ec3382fe703d0164c8a122c
allerria <wtf496@gmail.com>
2020-08-03 09:02:16 +0300

    adjust error handling to unique interface.

--- service/service.go
+++ service/service.go
@@ -47,13 +47,31 @@ type ErrorResponse struct {
 	Message string `json:"message"`
 }
 
+type appError struct {
+	Error   error
+	Message string
+	Code    int
+}
+
+type appHandler func(http.ResponseWriter, *http.Request) *appError
+
+func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if e := fn(w, r); e != nil {
+		log.Println(e.Error)
+		http.Error(w, e.Message, e.Code)
+	}
+}
+
 func CreateRouter(s *Service) *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/users/add", s.createUserHandler).Methods(http.MethodPost)
-	r.HandleFunc("/chats/add", s.creatChatHandler).Methods(http.MethodPost)
-	r.HandleFunc("/chats/get", s.getUserChatsHandler).Methods(http.MethodPost)
-	r.HandleFunc("/messages/add", s.createMessageHandler).Methods(http.MethodPost)
-	r.HandleFunc("/messages/get", s.getChatMessagesHandler).Methods(http.MethodPost)
+	ur := r.PathPrefix("/users").Subrouter()
+	ur.HandleFunc("/add", appHandler(s.createUserHandler).ServeHTTP).Methods(http.MethodPost)
+	cr := r.PathPrefix("/chats").Subrouter()
+	cr.HandleFunc("/add", appHandler(s.creatChatHandler).ServeHTTP).Methods(http.MethodPost)
+	cr.HandleFunc("/get", appHandler(s.getUserChatsHandler).ServeHTTP).Methods(http.MethodPost)
+	mr := r.PathPrefix("/messages").Subrouter()
+	mr.HandleFunc("/add", appHandler(s.createMessageHandler).ServeHTTP).Methods(http.MethodPost)
+	mr.HandleFunc("/get", appHandler(s.getChatMessagesHandler).ServeHTTP).Methods(http.MethodPost)
 	return r
 }
 
@@ -65,230 +83,147 @@ func ParseConfig() (*ConfigService, error) {
 	return cfg, nil
 }
 
-func (s *Service) createUserHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Service) createUserHandler(w http.ResponseWriter, r *http.Request) *appError {
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't read request body."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	data := CreateUserRequestBody{}
 	if err := json.Unmarshal(body, &data); err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't unmarshal request."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	id, err := s.Model.CreateUser(data.Username)
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't create user."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	msg, err := json.Marshal(map[string]string{"id": id})
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"User have been created, but can't marshal id."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(msg)
+	return nil
 }
 
-func (s *Service) creatChatHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Service) creatChatHandler(w http.ResponseWriter, r *http.Request) *appError {
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't read request body."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	data := CreateChatRequestBody{}
 	if err := json.Unmarshal(body, &data); err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't unmarshal request."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	id, err := s.Model.CreateChat(data.Name, data.Users)
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't create chat."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	msg, err := json.Marshal(map[string]uint64{"id": id})
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Chat created, but can't marshal id."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(msg)
+	return nil
 }
 
-func (s *Service) createMessageHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Service) createMessageHandler(w http.ResponseWriter, r *http.Request) *appError {
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't read request body."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	data := CreateMessageRequestBody{}
 	if err := json.Unmarshal(body, &data); err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't unmarshal request."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 	var chatID int
 	chatID, err = strconv.Atoi(data.Chat)
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! ChatID is incorrect."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	id, err := s.Model.CreateMessage(uint64(chatID), data.Author, data.Text)
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't create message."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	msg, err := json.Marshal(map[string]uint64{"id": id})
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Message created, but can't unmarshal id."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(msg)
+	return nil
 }
 
-func (s *Service) getUserChatsHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Service) getUserChatsHandler(w http.ResponseWriter, r *http.Request) *appError {
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't read request body."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	data := GetUserChatsRequestBody{}
 	if err := json.Unmarshal(body, &data); err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't unmarshal request."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	chats, err := s.Model.GetUserChats(data.ID)
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't get user chats."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	msg, err := json.Marshal(chats)
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't marshal chats."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(msg)
+	return nil
 }
 
-func (s *Service) getChatMessagesHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Service) getChatMessagesHandler(w http.ResponseWriter, r *http.Request) *appError {
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't read request body."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	data := GetChatMessagesRequestBody{}
 	if err := json.Unmarshal(body, &data); err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't unmarshal request."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 	var chatID int
 	chatID, err = strconv.Atoi(data.Chat)
 	if err := json.Unmarshal(body, &data); err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! ChatID is incorrect."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	chats, err := s.Model.GetChatMessages(uint64(chatID))
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't get messages."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	msg, err := json.Marshal(chats)
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		msg, _ := json.Marshal(ErrorResponse{"Error! Can't marshal messages."})
-		w.Write(msg)
-		return
+		return &appError{err, err.Error(), http.StatusInternalServerError}
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(msg)
+	return nil
 }

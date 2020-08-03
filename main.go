e9458e1fc1362b86ddb048d76ea3557e219a3f9e
allerria <wtf496@gmail.com>
2020-08-03 14:49:56 +0300

    remove debug env variables settings.

--- main.go
+++ main.go
@@ -3,19 +3,9 @@ package main
 import (
 	"github.com/allerria/backend-trainee-assignment/service"
 	"log"
-	"os"
 )
 
-// Для дебага на windows
-func setEnvVariables() {
-	os.Setenv("POSTGRES_USER", "allerria")
-	os.Setenv("POSTGRES_PASS", "root")
-	os.Setenv("POSTGRES_DATABASE", "messenger")
-	os.Setenv("SERVICE_PORT", "9000")
-}
-
 func main() {
-	setEnvVariables()
 	service, err := service.InitService()
 	if err != nil {
 		log.Fatal(err)
